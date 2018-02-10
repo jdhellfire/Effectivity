d5b08c11e372530616f479fc6ce48905f7104a3e
jdhellfire <jdhellfire@126.com>
2018-02-10 12:12:17 +0800

    RF : RF test case for clean code

--- Effectivity/Testing.py
+++ Effectivity/Testing.py
@@ -5,14 +5,16 @@ from DispJudger import DispJudger
 
 class FTOfJudger(unittest.TestCase):
 
+    def setUp(self):
+        self.judger = DispJudger()
+
     def test_001_step_show_judgemnet_by_range_any(self):
         """
         GIVEN : effect range : ANY
         WHEN  : input any airplane seq
         THEN  : return judge result true
         """
-        judger = DispJudger()
-        self.assertTrue(judger.is_show(1,'ANY'))
+        self.assertTrue(self.judger.is_show(1,'ANY'))
 
     def test_002_step_show_judgement_by_single_seq(self):
         """
@@ -20,8 +22,7 @@ class FTOfJudger(unittest.TestCase):
         WHEN  : input airplane seq equal effect range : 1
         THEN  : return judge result true
         """
-        judger = DispJudger()
-        self.assertTrue(judger.is_show(1,1))
+        self.assertTrue(self.judger.is_show(1,1))
 
 
 if __name__ == '__main__':
