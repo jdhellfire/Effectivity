90008924438979d2a9ac90f50bf9265452d271da
jdhellfire <jdhellfire@126.com>
2018-02-10 12:10:54 +0800

    RF : RF is_show method let case pass

--- Effectivity/DispJudger.py
+++ Effectivity/DispJudger.py
@@ -3,4 +3,6 @@ class DispJudger(object):
     def is_show(self,seq,range):
         if range == 'ANY':
             return True
-        return True
+
+        if seq == range:
+            return True
