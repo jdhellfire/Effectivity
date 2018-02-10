6726d29d9767b10f0445be044d58ebcd31f35067
jdhellfire <jdhellfire@126.com>
2018-02-10 12:00:44 +0800

    RED : add test 001 of judgement range ANY, run fail

--- Effectivity/Testing.py
+++ Effectivity/Testing.py
@@ -2,7 +2,17 @@ import unittest
 
 
 class FTOfJudger(unittest.TestCase):
-    pass
+
+    def test_001_step_show_judgemnet_by_range_any(self):
+        """
+        GIVEN : effect range : ANY
+        WHEN  : input any airplane seq
+        THEN  : return judge result true
+        """
+        judger = DisplayJudger()
+        self.assertTrue(1,'ANY')
+
+
 
 
 if __name__ == '__main__':
