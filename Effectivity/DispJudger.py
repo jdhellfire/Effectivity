8b6dec704969951d13f7afe12d7dcf7cc826d03a
jdhellfire <jdhellfire@126.com>
2018-02-10 13:50:35 +0800

    RF : RF to function program style !

--- Effectivity/DispJudger.py
+++ Effectivity/DispJudger.py
@@ -3,11 +3,8 @@ from functools import reduce
 
 class DispJudger(object):
     def is_show(self, seq, effect_elemts):
-        if effect_elemts == 'ANY':
-            return True
 
-        ret = reduce(lambda x, y: x | y,
-                     map(lambda elemt: True if seq in list(elemt if isinstance(elemt, range) else [elemt]) else False,
+        return True if effect_elemts == 'ANY' \
+                    else reduce(lambda x, y: x | y,
+                         map(lambda elemt: True if seq in list(elemt if isinstance(elemt, range) else [elemt]) else False,
                          effect_elemts))
-
-        return ret
